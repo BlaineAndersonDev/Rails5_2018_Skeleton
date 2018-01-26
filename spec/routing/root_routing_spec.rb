e851d08cfe77561b653b8637aac00f4868ed3207
Blaine Anderson <BlaineA97@Gmail.com>
2018-01-26 16:14:46 -0700

    added UsersRoutes to rspec routing tests with TDD passing tests. Minor adjustments to RootRoutes test title.

--- spec/routing/root_routing_spec.rb
+++ spec/routing/root_routing_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Root Route", :type => :routing do
+RSpec.describe "RootRoutes", :type => :routing do
   it "#root" do
     expect(get("/")).
     to route_to('static_pages#home')
