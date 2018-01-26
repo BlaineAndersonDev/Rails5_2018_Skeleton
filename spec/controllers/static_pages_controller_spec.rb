e4831b4f5689f78d4cbdc9160f8707bbda715ca9
Blaine Anderson <BlaineA97@Gmail.com>
2018-01-25 19:15:15 -0700

    Added failing test for about page as example

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -16,4 +16,11 @@ RSpec.describe StaticPagesController, type: :controller do
     end
   end
 
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
