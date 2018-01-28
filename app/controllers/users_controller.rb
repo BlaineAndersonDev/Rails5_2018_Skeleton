2b5effdb35cefb21ccd321872f1ed46609c3e2f2
Blaine Anderson <BlaineA97@Gmail.com>
2018-01-28 15:56:01 -0700

    Added private param hashing for new User information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,18 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
