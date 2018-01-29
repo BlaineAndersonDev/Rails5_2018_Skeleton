307002f1262275e616e5b444c9c4a6dd56781254
Blaine Anderson <BlaineA97@Gmail.com>
2018-01-28 22:44:13 -0700

    Updated RSpec tests to be more verbose and simplified context

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   let!(:only_user) { User.create(:name => "Blaine", :email => "BlaineEmail123@gmail.com", :password => "BlainesAwesome", :password_confirmation => "BlainesAwesome") }
 
-  describe "GET #new" do
+  describe "GET user#new" do
     before { get :index }
 
     it "assigns @users" do
@@ -15,7 +15,7 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  describe "GET 'show'" do
+  describe "GET user#show" do
     before { get :show, params: {id: only_user.id} }
 
     it "assigns @user" do
@@ -24,6 +24,18 @@ RSpec.describe UsersController, type: :controller do
 
     it "renders the show template" do
       expect(response).to render_template("show")
+    end
+  end
+
+  describe "GET 'new'" do
+    before { get :new }
+
+    it "assigns @post" do
+      expect(assigns(:post)).to be_a_new(Post)
+    end
+
+    it "renders the new template" do
+      expect(response).to render_template("new")
     end
   end
 
