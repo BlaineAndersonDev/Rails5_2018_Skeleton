ad8f24f525a0032baa1554c166eac5422473f0ad
Blaine Anderson <BlaineA97@Gmail.com>
2018-01-28 22:25:46 -0700

    Created user show page rspec tests, all green

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  let(:only_user) { User.create(:name => "Blaine", :email => "BlaineEmail123@gmail.com", :password => "BlainesAwesome", :password_confirmation => "BlainesAwesome") }
+  let!(:only_user) { User.create(:name => "Blaine", :email => "BlaineEmail123@gmail.com", :password => "BlainesAwesome", :password_confirmation => "BlainesAwesome") }
 
   describe "GET #new" do
     before { get :index }
@@ -12,6 +12,18 @@ RSpec.describe UsersController, type: :controller do
 
     it "renders the index template" do
       expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET 'show'" do
+    before { get :show, params: {id: only_user.id} }
+
+    it "assigns @user" do
+      expect(assigns(:user)).to eq(only_user)
+    end
+
+    it "renders the show template" do
+      expect(response).to render_template("show")
     end
   end
 
